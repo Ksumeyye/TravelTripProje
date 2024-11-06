e4eedea9a6e60ab8525ea7e65d4a0665b61a7c11
ksumeyye <sumeyyeekaya@outlook.com>
2024-11-06 22:07:55 +0300

    Hakkımızda kısmını tamamladım.

---
+++ TravelTripProje/Models/Siniflar/Blog.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Web;
+
+namespace TravelTripProje.Models.Siniflar
+{
+    public class Blog
+    {
+        [Key]
+        public int ID { get; set; }
+        public int Baslik { get; set; }
+        public DateTime Tarih { get; set; }
+        public string Aciklama { get; set; }
+        public string BlogImage { get; set; }
+        public ICollection<Yorumlar> Yorumlars { get; set; }//İnterface kümeleme. Bir blogun birden fazla yorumu olabilir.C# Interface, yapısı gereği diğer sınıflara yol gösterici,
+                                                            //rehberlik yapmak için oluşturulan, kendisinden implement edilen bir sınıfa doldurulması zorunlu olan bazı özelliklerin
+                                                            //aktarılmasını sağlayan bir kavramdır.
+    }
+}
---
+++ TravelTripProje/Models/Siniflar/Context.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Data.Entity;
+
+namespace TravelTripProje.Models.Siniflar
+{
+    
+    public class Context:DbContext //ilgilitablolara ulaşabilmek için dbcontext'ten miras alınır.
+    {
+        public DbSet<Admin> Admins { get; set; } //Adminin veritabanındaki ismi admins olur.
+        public DbSet<AdresBlog> AdresBlogs { get; set; } //AdresBlogun veritabanındaki ismi AdresBlogs olur.
+        public DbSet<Blog> Blogs { get; set; }
+        public DbSet<Hakkimizda> Hakkimizdas { get; set; }
+        public DbSet<iletisim> iletisims { get; set; }
+        public DbSet<Yorumlar> Yorumlars { get; set; }
+    }
+}
