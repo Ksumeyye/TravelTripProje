74de20e74ace7d05df138bdace014d2b9465751a
ksumeyye <sumeyyeekaya@outlook.com>
2024-11-08 00:58:36 +0300

    Son Blogları tamamladım.

--- TravelTripProje/Controllers/BlogController.cs
+++ TravelTripProje/Controllers/BlogController.cs
@@ -10,16 +10,23 @@ namespace TravelTripProje.Controllers
     public class BlogController : Controller
     {
         Context b=new Context();
+        BlogYorum by = new BlogYorum();// BlogYorum'dan by adında bir nesne türetiyorum.
         // GET: Blog
         public ActionResult Index()
         {
-            var bloglar = b.Blogs.ToList();
-            return View(bloglar);
+            by.Deger1=b.Blogs.ToList();
+            by.Deger3 = b.Blogs.Take(3).ToList();
+            //var bloglar = b.Blogs.ToList();
+            return View(by);
         }
+       
         public ActionResult BlogDetay(int? id)
         {
-            var blogbul = b.Blogs.Where(x => x.ID == id).ToList();
-            return View(blogbul);
+            //var blogbul = b.Blogs.Where(x => x.ID == id).ToList();
+            by.Deger1=b.Blogs.Where(x=>x.ID==id).ToList();
+            by.Deger2 =b.Yorumlars.Where(x => x.ID == id).ToList();
+            return View(by);
         }
+
     }
 }
